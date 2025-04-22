55e3824f5c7fc6d8f6387d2170e82c49288b860f
Artem Alekseyev <alekseevartem586@gmail.com>
2025-04-22 16:42:19 +0300

    Change get_all_student function to retutn more readable students list

--- middle.py
+++ middle.py
@@ -6,8 +6,8 @@ def add_student(msg):
 def get_all_students():
     list_student = database.db_get_all_sort_studnet_list()
 
-    out_str = "ID, NAME\n---\n"
+    out_str = "ID | NAME\n\n"
     for i in list_student:
-        out_str += f"{i[0]}, {i[1]}\n---\n"
+        out_str += f"{i[0]} | {i[1]}\n"
 
     return out_str
